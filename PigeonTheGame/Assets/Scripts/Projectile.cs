e7f436559f89aa7d6a35b354ee2cc3af86e1a6da
VeryHotShark <nguyen.tu.tony2000@gmail.com>
2018-10-15 13:36:01 +0200

    Added projectile ricochet

--- PigeonTheGame/Assets/Scripts/Projectile.cs
+++ PigeonTheGame/Assets/Scripts/Projectile.cs
@@ -11,9 +11,13 @@ public class Projectile : MonoBehaviour
 
     private GameObject m_objectShotFrom;
 
+    float myForce;
+
     // Use this for initialization
     public void OnProjectileSpawn(Vector3 dir, float force, int damage, float lifeTime, GameObject objectShotFrom)
     {
+        myForce = force;
+
 		GetComponents();
         m_objectShotFrom = objectShotFrom;
         m_damage = damage;
@@ -26,8 +30,22 @@ public class Projectile : MonoBehaviour
 		m_rigid = GetComponent<Rigidbody>();
 	}
 
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision other) // ZMIEN NA RAYCAST, żeby to był projectile zamiast bullet albo pól na pól, że leci sobie i raycast jest na początku Bulletu i on wykrywa zamiast Kolizji
     {
+        
+        ContactPoint contactPoint = other.contacts[0];
+
+        Vector3 reflectDir = Vector3.Reflect(transform.forward, contactPoint.normal);
+
+        m_rigid.velocity = Vector3.zero;
+
+        Debug.Log(m_rigid.velocity);
+        Debug.DrawLine(contactPoint.point,reflectDir,Color.red, 2f);
+
+        float rotation = 90 - Mathf.Atan2(reflectDir.z,reflectDir.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0f,rotation, 0f);
+        m_rigid.AddForce(reflectDir * myForce, ForceMode.Impulse);
+
         Health otherHealth = other.gameObject.GetComponent<Health>();
 
         if(otherHealth != null && other.gameObject != m_objectShotFrom)
