5682e6a6714a0a60b0b5637f99518bc184155530
VeryHotShark <nguyen.tu.tony2000@gmail.com>
2018-11-15 15:25:07 +0100

    Added TIme Manager
    Slow downs time when we kill someone

---
+++ PigeonTheGame/Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+    [Range(0f, 1f)]
+    public float slowDownValue;
+
+    public float slowDownDuration;
+    public float returnDuration;
+
+    float m_defaultTimeScale = 1f;
+
+    IEnumerator m_slowDownRoutine;
+
+    // Use this for initialization
+    void Start()
+    {
+		EnemyHealth.OnAnyEnemyDeath += StartSlowDown;
+    }
+
+    void StartSlowDown()
+    {
+		m_slowDownRoutine = ChangeTimeRoutine();
+
+		if(m_slowDownRoutine != null)
+			StartCoroutine(m_slowDownRoutine);
+    }
+
+    IEnumerator ChangeTimeRoutine()
+    {
+        float percent = 0f;
+        float speed = 1f / slowDownDuration;
+
+        float slowTimeScale = Time.timeScale * slowDownValue;
+
+        while (percent < 1f)
+        {
+            percent += Time.unscaledDeltaTime * speed;
+            Time.timeScale = Mathf.Lerp(slowTimeScale, 1f, percent);
+            yield return null;
+        }
+
+    }
+}
